7a60955527a9bd0b647bb1dd87a5994b08be07d0
ahmed osman <ahmedos099912@gmail.com>
2023-11-22 14:51:03 +0200

    home page edit

--- lib/presentation/Screens/home_page.dart
+++ lib/presentation/Screens/home_page.dart
@@ -67,103 +67,131 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Enjoy the ride in the',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium!
-                    .copyWith(color: Colors.grey, fontSize: 16),
-              ),
-              Text(
-                'Car you Like.',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(fontSize: 36),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Center(
-                child: Container(
-                  width: MediaQuery.sizeOf(context).width * 0.9,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 3,
-                        blurRadius: 5,
-                        offset:
-                            const Offset(0, 2), // changes position of shadow
-                      ),
-                    ],
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8),
-                    child: DropdownButtonFormField(
-                      decoration: InputDecoration(
-                        focusedBorder: const UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromARGB(255, 201, 201, 201))),
-                        enabledBorder: const UnderlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromARGB(255, 201, 201, 201))),
-                        prefixIcon: Icon(
-                          Icons.location_on,
-                          color: Theme.of(context).colorScheme.secondary,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Enjoy the ride in the',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(color: Colors.grey, fontSize: 16),
+                ),
+                Text(
+                  'Car you Like.',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(fontSize: 36),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Container(
+                    width: MediaQuery.sizeOf(context).width * 0.9,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 3,
+                          blurRadius: 5,
+                          offset:
+                              const Offset(0, 2), // changes position of shadow
                         ),
+                      ],
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: DropdownButtonFormField(
+                        decoration: InputDecoration(
+                          focusedBorder: const UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 201, 201, 201))),
+                          enabledBorder: const UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 201, 201, 201))),
+                          prefixIcon: Icon(
+                            Icons.location_on,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                        value: dropdownvalue,
+                        isExpanded: true,
+                        items: items
+                            .map((String e) =>
+                                DropdownMenuItem(value: e, child: Text(e)))
+                            .toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownvalue = newValue!;
+                          });
+                        },
                       ),
-                      value: dropdownvalue,
-                      isExpanded: true,
-                      items: items
-                          .map((String e) =>
-                              DropdownMenuItem(value: e, child: Text(e)))
-                          .toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          dropdownvalue = newValue!;
-                        });
-                      },
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              rowdata(context, 'Best Offers', 'view all'),
-              const SizedBox(
-                height: 20,
-              ),
-              // carShow(context, 'assets/imgs/car2.png', '4.5', 'Mercedes Benz',
-              //     'Sedan', '\$150', '4', 'Automatic', '\$129')
-              SizedBox(
-                height: MediaQuery.sizeOf(context).height / 2.5,
-                width: MediaQuery.sizeOf(context).width,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: discontedCars.length,
-                    itemBuilder: (context, index) {
-                      return carShow(
-                          context,
-                          discontedCars[index].img,
-                          discontedCars[index].rating,
-                          discontedCars[index].name,
-                          discontedCars[index].brand,
-                          discontedCars[index].disconted,
-                          discontedCars[index].seats,
-                          discontedCars[index].gear,
-                          discontedCars[index].price);
-                    }),
-              )
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                rowdata(context, 'Best Offers', 'view all'),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 2.5,
+                  width: MediaQuery.sizeOf(context).width,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: discontedCars.length,
+                      itemBuilder: (context, index) {
+                        return carShow(
+                            context,
+                            discontedCars[index].img,
+                            discontedCars[index].rating,
+                            discontedCars[index].name,
+                            discontedCars[index].brand,
+                            discontedCars[index].disconted,
+                            discontedCars[index].seats,
+                            discontedCars[index].gear,
+                            discontedCars[index].price);
+                      }),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                rowdata(context, 'New Cars', 'view all'),
+                const SizedBox(
+                  height: 20,
+                ),
+                // carShow(context, 'assets/imgs/car2.png', '4.5', 'Mercedes Benz',
+                //     'Sedan', '\$150', '4', 'Automatic', '\$129')
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 2.5,
+                  width: MediaQuery.sizeOf(context).width,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: discontedCars.length,
+                      itemBuilder: (context, index) {
+                        return carShow2(
+                            context,
+                            discontedCars[index].img,
+                            discontedCars[index].rating,
+                            discontedCars[index].name,
+                            discontedCars[index].brand,
+                            discontedCars[index].disconted,
+                            discontedCars[index].seats,
+                            discontedCars[index].gear,
+                            discontedCars[index].price);
+                      }),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/presentation/components/home_page_components.dart
+++ lib/presentation/components/home_page_components.dart
@@ -168,3 +168,179 @@ Widget carShow(BuildContext context, String img, String rating, String name,
     ),
   );
 }
+
+Widget carShow2(BuildContext context, String img, String rating, String name,
+    String brand, String disconted, String seats, String gear, String price) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      width: MediaQuery.sizeOf(context).width * 0.8,
+      // height: MediaQuery.sizeOf(context).height / 2.5,
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.3),
+          spreadRadius: 3,
+          blurRadius: 5,
+          offset: const Offset(0, 2), // changes position of shadow
+        ),
+      ], color: Colors.white, borderRadius: BorderRadius.circular(5)),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Color.fromARGB(111, 255, 109, 73),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                        Text(rating,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(fontSize: 14))
+                      ],
+                    ),
+                    Icon(
+                      Icons.favorite_border,
+                      color: Theme.of(context).colorScheme.primary,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Color.fromARGB(255, 255, 109, 73),
+              height: 30,
+            ),
+            Expanded(
+                child: Container(
+                    decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromARGB(159, 255, 109, 73),
+                          spreadRadius: 1,
+                          blurRadius: 15,
+                          offset: Offset(5, 5),
+                        ),
+                        BoxShadow(
+                            color: Colors.white,
+                            offset: Offset(-5, -5),
+                            blurRadius: 15,
+                            spreadRadius: 1),
+                      ],
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color.fromARGB(73, 255, 109, 73),
+                          Color.fromARGB(94, 255, 109, 73),
+                          Color.fromARGB(119, 255, 109, 73),
+                          Color.fromARGB(159, 255, 109, 73),
+                        ],
+                      ),
+                    ),
+                    child: Center(child: Image.asset(img)))),
+            Text(
+              name,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(fontSize: 22),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  brand,
+                  style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                      fontSize: 16,
+                      color: const Color.fromARGB(255, 138, 138, 138)),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      disconted,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          decoration: TextDecoration.lineThrough,
+                          fontSize: 20,
+                          color: const Color.fromARGB(255, 138, 138, 138)),
+                    ),
+                    const SizedBox(
+                      width: 35,
+                    )
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.person,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    Text(
+                      seats,
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayLarge!
+                          .copyWith(fontSize: 16),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Icon(
+                      Icons.g_mobiledata_rounded,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    Text(
+                      gear,
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayLarge!
+                          .copyWith(fontSize: 16),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      price,
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(fontSize: 22),
+                    ),
+                    Text(
+                      '/ Day',
+                      style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                          fontSize: 12,
+                          color: const Color.fromARGB(255, 138, 138, 138)),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    ),
+  );
+}
