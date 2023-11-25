f39cc7eeec64a2e1895a79b2568d38fc71344d91
ahmed osman <ahmedos099912@gmail.com>
2023-11-25 20:37:03 +0200

    HISTROY PAGE EDIT

--- lib/presentation/Screens/history.dart
+++ lib/presentation/Screens/history.dart
@@ -45,8 +45,93 @@ class History extends StatelessWidget {
                     const Icon(
                       Icons.settings,
                       size: 30,
-                    )
+                    ),
                   ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Maserati Convertibale',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(fontSize: 20),
+                        ),
+                        const Text(
+                          'cabriotlet',
+                          style: TextStyle(fontSize: 14, color: Colors.black54),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      'order date: ',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleLarge!
+                                          .copyWith(
+                                              fontSize: 18,
+                                              color: Colors.black45),
+                                    ),
+                                    const Text(
+                                      'Oct 29, 2022',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.black87,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Status: ',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleLarge!
+                                          .copyWith(
+                                              fontSize: 18,
+                                              color: Colors.black45),
+                                    ),
+                                    Text(
+                                      'Active',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.green[400],
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              width: MediaQuery.sizeOf(context).width / 3,
+                              // height: MediaQuery.sizeOf(context).width / 3,
+                              child: Image.asset('assets/imgs/car1.png'),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 )
               ],
             ),
